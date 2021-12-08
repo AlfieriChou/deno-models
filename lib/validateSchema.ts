80e68a96f513f5c64b34e082333cd2121458c933
zhoudeyou <alfierichou@gmail.com>
2021-12-08 10:44:09 +0800

    feat: validate schema

---
+++ lib/validateSchema.ts
@@ -0,0 +1,15 @@
+import Ajv, { ErrorObject } from "ajv"
+
+const ajv: Ajv = new Ajv()
+
+const validateSchema = (schema: Object, data: Object): void | Error => {
+  const isValid: boolean = ajv.validate(schema, data)
+  if (!isValid) {
+    const errMsg = ajv.errors!.map(
+      (errorMsg: ErrorObject) => (JSON.stringify(errorMsg))
+    ).join()
+    throw new Error(errMsg)
+  }
+}
+
+export default validateSchema
